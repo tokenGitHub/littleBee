2062cdef5c87485d21962c186a7da4ee90603beb
Xian鱼 <hanhe12333@qq.com>
2019-03-28 15:38:27 +0800

    feature: 添加登录过滤器，验证用户是否已经登录

--- src/main/java/com/littleBee/bee/filter/LoginFilter.java
+++ src/main/java/com/littleBee/bee/filter/LoginFilter.java
@@ -1,4 +1,41 @@
 package com.littleBee.bee.filter;
 
-public class LoginFilter {
+import com.littleBee.bee.service.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter
+public class LoginFilter implements Filter {
+    @Autowired
+    RedisService redisService;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String userCode = servletRequest.getParameter("userCode");
+        String userName = servletRequest.getParameter("userName");
+
+        if(userCode == null || userName == null){
+            servletResponse.getWriter().println(new Exception("未登录"));
+        }else{
+            String redisUserCode = redisService.getUserLoginCode(userName);
+            if(redisUserCode == null || !redisUserCode.equals(userCode)){
+                servletResponse.getWriter().println(new Exception("登录过期或登录信息错误"));
+            }else{
+                filterChain.doFilter(servletRequest, servletResponse);
+            }
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
 }
