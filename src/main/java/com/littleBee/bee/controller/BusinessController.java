df0ac68e2a0071d2175559a58672a1fe3ea4c7e4
yun <hanhe12333@qq.com>
2019-05-09 16:57:20 +0800

    feature: 添加接口注释

--- src/main/java/com/littleBee/bee/controller/BusinessController.java
+++ src/main/java/com/littleBee/bee/controller/BusinessController.java
@@ -22,8 +22,21 @@ public class BusinessController {
     @Autowired
     private UserService userService;
 
-
-    @PostMapping("register")
+    /**
+     *
+     * @param userName 用户名
+     * @param companyName   公司名
+     * @param industry      所属行业
+     * @param companyIntroduce  公司介绍
+     * @param password      密码
+     * @param email         公司邮箱
+     * @param realName      用户真实姓名
+     * @param tele          公司电话
+     * @param sex           注册人性别
+     * @param verification  验证码
+     * @return  返回公司所有信息
+     */
+    @GetMapping("register")
     public Object userRegister(@RequestParam String userName,
                                @RequestParam String companyName,
                                @RequestParam String industry,
@@ -31,12 +44,12 @@ public class BusinessController {
                                @RequestParam String password,
                                @RequestParam String email,
                                @RequestParam String realName,
-                               @RequestParam String para,
+                               @RequestParam String tele,
                                @RequestParam int sex,
                                @RequestParam String verification){
 
         if(verification.equals(redisService.getEmailVerificationCode(email))) {
-            User user = parseUserByData(userName, password, email, realName, sex, para, companyName, industry, companyIntroduce);
+            User user = parseUserByData(userName, password, email, realName, sex, tele, companyName, industry, companyIntroduce);
             userService.insertUser(user);
             return JsonUtils.getSuccessResult(user);
         }else {
@@ -44,7 +57,23 @@ public class BusinessController {
         }
     }
 
-    @PostMapping("releaseFullTimeWork")
+    /**
+     *
+     * @param userId     发布者id，来自header
+     * @param identity   职位类型（0兼职，1全职）
+     * @param needPerson 需要人数
+     * @param wages      工资
+     * @param workTime   工作时间
+     * @param province   省
+     * @param city       市
+     * @param duty       工作职责
+     * @param requirement   工作要求
+     * @param contacts      联系人
+     * @param tele          联系电话
+     * @param remarks       备注
+     * @return  求职信息是否发布成功
+     */
+    @GetMapping("releaseFullTimeWork")
     public Object releaseFullTimeWork(@RequestHeader("userId") int userId, @RequestParam int identity,
                                       @RequestParam int needPerson, @RequestParam String wages,
                                       @RequestParam String workTime, @RequestParam String province,
@@ -72,10 +101,16 @@ public class BusinessController {
         work.setRequirement(requirement);
 
         workService.saveWork(work);
-        return JsonUtils.getSuccessResult("求职信息发布成功");
+        return JsonUtils.getSuccessResult("职位信息发布成功");
     }
 
-    @PostMapping("listAllReleaseWork")
+    /**
+     *
+     * @param userId    商家id
+     * @param identity  职位类型 工作类型（0兼职，1全职）
+     * @return  返回该商家发布的所有对应职业类型的职位
+     */
+    @GetMapping("listAllReleaseWork")
     public Object listAllReleaseWork(@RequestHeader("userId") int userId, @RequestParam("identity") int identity){
         User user = userService.selectUserById(userId);
         if(user == null ){
@@ -87,7 +122,14 @@ public class BusinessController {
         return JsonUtils.getSuccessResult(workList);
     }
 
-    @PostMapping("listPersonForWork")
+
+    /**
+     *
+     * @param userId 商家id
+     * @param workId 职位id
+     * @return  返回该商家发布的对应职位，的所有报名人的信息
+     */
+    @GetMapping("listPersonForWork")
     public Object listUserForWork(@RequestHeader("userId") int userId, @RequestParam("workId") int workId){
         User user = userService.selectUserById(userId);
         if(checkBusiness(user)){
