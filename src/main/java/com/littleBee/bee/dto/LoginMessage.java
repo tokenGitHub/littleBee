bd067c01acbcb38134283c434846b92825596410
yun <hanhe12333@qq.com>
2019-05-09 10:21:10 +0800

    bugfix:修改登陆信息dto没有getter，setter方法bug

--- src/main/java/com/littleBee/bee/dto/LoginMessage.java
+++ src/main/java/com/littleBee/bee/dto/LoginMessage.java
@@ -1,10 +1,14 @@
 package com.littleBee.bee.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@AllArgsConstructor
+@Data
+@EntityScan
 @NoArgsConstructor
+@AllArgsConstructor
 public class LoginMessage {
     private int id;
     private String token;
