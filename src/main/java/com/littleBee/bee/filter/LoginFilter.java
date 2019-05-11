106bc9dc7acf837068e4fbd6d5cd501817ece41b
yun <hanhe12333@qq.com>
2019-05-11 15:26:21 +0800

    bugfix: 解决跨域问题

--- src/main/java/com/littleBee/bee/filter/LoginFilter.java
+++ src/main/java/com/littleBee/bee/filter/LoginFilter.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,22 +26,29 @@ public class LoginFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
         log.info(request.getRequestURI());
+        //以下三行解决跨域问题
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with,Authorization,content-type,token, userId");
+        response.setHeader("Access-Control-Allow-Credentials","true");
 
         if(whiteList(request.getRequestURI())){
             filterChain.doFilter(servletRequest, servletResponse);
             return;
         }
 
-        String userCode = servletRequest.getParameter("userCode");
-        String userName = servletRequest.getParameter("userName");
+        String token = request.getHeader("token");
+        String userId = request.getHeader("userId");
 
 
-        if(userCode == null || userName == null){
+        if(token == null || userId == null){
             servletResponse.getWriter().println(new Exception("未登录"));
         }else{
-            String redisUserCode = redisService.getUserLoginCode(userName);
-            if(redisUserCode == null || !redisUserCode.equals(userCode)){
+            String redisUserCode = redisService.getUserLoginCode(userId);
+            if(redisUserCode == null || !redisUserCode.equals(token)){
                 servletResponse.getWriter().println(new Exception("登录过期或登录信息错误"));
             }else{
                 filterChain.doFilter(servletRequest, servletResponse);
